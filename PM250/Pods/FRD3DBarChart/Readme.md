18b1765bed9c7dfacce327c6897b9a568a63a8c3
WayneWeiZhang <waynezhang@me.com>
2013-12-12 16:47:18 +0800

    add

---
+++ PM250/Pods/FRD3DBarChart/Readme.md
@@ -0,0 +1,13 @@
+FRD3DBarChart
+
+iOS 3D bar chart control made with GLKit.
+
+Sebastien Windal - Free Range Developers.
+
+Licensed under New BSD.
+
+See http://windal.net/blog/?p=153 for documentation.
+
+![Screenshot](FRD3DBarChart/raw/master/screenshot2.png) 
+
+
---
+++ PM250/Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AFNetworking
+#define COCOAPODS_POD_AVAILABLE_AFNetworking
+#define COCOAPODS_VERSION_MAJOR_AFNetworking 1
+#define COCOAPODS_VERSION_MINOR_AFNetworking 3
+#define COCOAPODS_VERSION_PATCH_AFNetworking 3
+
+// FRD3DBarChart
+#define COCOAPODS_POD_AVAILABLE_FRD3DBarChart
+#define COCOAPODS_VERSION_MAJOR_FRD3DBarChart 1
+#define COCOAPODS_VERSION_MINOR_FRD3DBarChart 0
+#define COCOAPODS_VERSION_PATCH_FRD3DBarChart 0
+
